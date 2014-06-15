15151f1aef988737335c0b5338b4a73d8f652728
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2014-06-15 18:37:42 +1000

    Converted train to use new goodness

--- train.go
+++ train.go
@@ -2,42 +2,44 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 
 	"github.com/kirillrdy/nadeshiko"
 )
 
 type Train struct {
-	x, y float64
+	Id       string
+	Position Point
+}
+
+func NewTrain() Train {
+	random := rand.Int63()
+	return Train{Id: fmt.Sprintf("%d", random)}
+}
+
+func (train Train) CssSelector() string {
+	return "#" + train.Id
 }
 
 func (train Train) AppendToPage(conneciton *nadeshiko.Connection) {
-	conneciton.JQuery("body").Append("<p id='train'>Hello</p>")
-	conneciton.JQuery("#train").SetCss("position", "absolute")
+	conneciton.JQuery("body").Append(fmt.Sprintf("<p id='%s'>Hello</p>", train.Id))
+	conneciton.JQuery(train.CssSelector()).SetCss("position", "absolute")
 }
 
 func (train *Train) Step(point Point) {
-	if train.x < point.x {
-		train.x += 1
-		x_diff := (point.x - train.x)
-		if x_diff != 0 {
-			delta := (point.y - train.y) / x_diff
-			train.y += delta
-		}
-	}
+	direction_vector := Line{train.Position, point}
+	fmt.Printf("Facing direction %f\n", direction_vector.AngleDegrees())
+	train.Position = train.Position.Transform(direction_vector.AngleRadians(), VELOCITY)
 }
 
 func (train Train) At(point Point) bool {
-	val := train.x == point.x && train.y == point.y
-	fmt.Printf("%b %f %f %f %f  \n", val, train.x, point.x, train.y, point.y)
+	val := train.Position.DistanceTo(point) < 0.5
+	fmt.Printf("%b %v %v \n", val, train.Position.Floored(), point.Floored())
 	return val
 }
 
-func (train *Train) Move(point Point) {
-	train.x = point.x
-	train.y = point.y
-}
-
 func (train Train) Draw(conneciton *nadeshiko.Connection) {
-	conneciton.JQuery("#train").SetCss("left", fmt.Sprintf("%fpx", train.x))
-	conneciton.JQuery("#train").SetCss("top", fmt.Sprintf("%fpx", train.y))
+	selector := "#" + train.Id
+	conneciton.JQuery(selector).SetCss("left", fmt.Sprintf("%fpx", train.Position.x))
+	conneciton.JQuery(selector).SetCss("top", fmt.Sprintf("%fpx", train.Position.y))
 }
