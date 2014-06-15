5beaea161c464419e74b7bf600890ee4cb0c686b
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2014-06-15 18:38:03 +1000

    New activity to demo some fancy stuff

--- main_activity.go
+++ main_activity.go
@@ -12,8 +12,15 @@ type MainActivity struct {
 
 func (activity MainActivity) Start(conneciton *nadeshiko.Connection) {
 
-	points := []Point{
-		Point{100, 10},
+	path := []Point{
+		Point{0, 0},
+		Point{30, 0},
+		Point{30, 40},
+		Point{200, 70},
+		Point{300, 50},
+		Point{330, 90},
+		Point{160, 200},
+		Point{200, 250},
 		Point{300, 300},
 		Point{10, 400},
 	}
@@ -22,22 +29,44 @@ func (activity MainActivity) Start(conneciton *nadeshiko.Connection) {
 			<path d="%s" fill="none" stroke="blue"/>
 			</svg> `
 
-	conneciton.JQuery("body").Append(fmt.Sprintf(data, pointsToPath(points)))
+	conneciton.JQuery("body").Append(fmt.Sprintf(data, Points(path).pointsSvgToPath()))
 
-	train := Train{}
+	button := `<input type="button" id="add" value="Add Train">`
+	conneciton.JQuery("body").Append(button)
+
+	conneciton.JQuery("#add").Click(func() {
+		addTrainToPath(path, conneciton)
+	})
+
+	go func() {
+		for {
+			addTrainToPath(path, conneciton)
+			time.Sleep(3000 * time.Millisecond)
+		}
+	}()
+
+}
+
+func addTrainToPath(path []Point, conneciton *nadeshiko.Connection) {
+
+	train := NewTrain()
 	train.AppendToPage(conneciton)
-
 	current_index := 0
 
 	go func() {
 		for {
 
-			train.Step(points[current_index])
-			train.Draw(conneciton)
-			time.Sleep(100 * time.Millisecond)
+			current_target_point := path[current_index]
 
-			if train.At(points[current_index]) {
+			train.Step(current_target_point)
+			train.Draw(conneciton)
+			time.Sleep(10 * time.Millisecond)
+
+			if train.At(current_target_point) {
 				current_index += 1
+				if current_index == len(path) {
+					return
+				}
 			}
 		}
 
