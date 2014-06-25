8907428e8e6f561fc8ada536c934b721d0953114
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2014-06-25 18:55:06 +1000

    Ident json

--- model/city.go
+++ model/city.go
@@ -1,7 +1,9 @@
 package model
 
 import (
+	"bytes"
 	"encoding/json"
+	"io/ioutil"
 	"log"
 	"os"
 )
@@ -25,11 +27,43 @@ func LoadCity(name string) (city City) {
 
 func (city City) Save(name string) {
 	json_file, err := os.Create(name)
-	defer json_file.Close()
 	if err != nil {
 		panic(err)
 	}
 
 	jsonEncoder := json.NewEncoder(json_file)
 	jsonEncoder.Encode(city)
+	json_file.Close()
+
+	IndentJson(name)
+}
+
+func LoadJson(name string) (data []byte) {
+	file, err := os.Open(name)
+	defer file.Close()
+	if err != nil {
+		log.Fatalf("ERROR %v \n", err)
+	}
+	data, err = ioutil.ReadAll(file)
+
+	if err != nil {
+		log.Fatalf("ERROR %v \n", err)
+	}
+
+	return data
+}
+
+func IndentJson(name string) {
+	data := LoadJson(name)
+	var buffer bytes.Buffer
+	json.Indent(&buffer, data, "", "  ")
+
+	file, err := os.Create(name)
+	if err != nil {
+		log.Fatalf("ERROR %v \n", err)
+	}
+	defer file.Close()
+
+	file.WriteString(buffer.String())
+
 }
